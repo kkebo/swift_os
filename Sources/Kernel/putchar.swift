f09bdd601b2f93bd5ffc3d11c6e6bac28881d2b4
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2024-01-15 01:10:31 +0900

    feat: support Raspberry Pi 3 Model B

--- Sources/Kernel/putchar.swift
+++ Sources/Kernel/putchar.swift
@@ -1,10 +1,33 @@
 import Volatile
 
-let uart = UnsafeMutablePointer<UInt8>(bitPattern: 0x09000000)!
+#if RASPI4
+    let uartBase: UInt = 0xFE201000
+#elseif RASPI3 || RASPI2
+    let uartBase: UInt = 0x3F201000
+#else
+    let uartBase: UInt = 0x20201000
+#endif
+
+let uartDR = UnsafeMutablePointer<UInt32>(bitPattern: uartBase)!
+let uartFR = UnsafePointer<UInt32>(bitPattern: uartBase + 0x18)!
+
+private func transmitFIFOFull() -> Bool {
+    volatile_load(uartFR) & (1 << 5) > 0
+}
+
+private func receiveFIFOEmpty() -> Bool {
+    volatile_load(uartFR) & (1 << 4) > 0
+}
 
 @_silgen_name("putchar")
 @discardableResult
 public func putchar(_ c: CInt) -> CInt {
-    volatile_store(uart, UInt8(c))
+    while transmitFIFOFull() {}
+    volatile_store(uartDR, UInt32(c))
     return c
+}
+
+func getchar() -> UInt8 {
+    while receiveFIFOEmpty() {}
+    return UInt8(volatile_load(uartDR))
 }
