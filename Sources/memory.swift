7129ba2daa268b5c18e739a445982e6920766df0
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2024-01-18 22:50:05 +0900

    refactor: remove `swift_release`

--- Sources/memory.swift
+++
@@ -1,5 +0,0 @@
-// FIXME: It shouldn't be needed.
-@_silgen_name("swift_release")
-public func swift_release(object: UnsafeMutablePointer<HeapObject>?) {
-    swift_release_n_(object: object, n: 1)
-}
