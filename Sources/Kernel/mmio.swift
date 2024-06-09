d2c43730f5ad35c8d4039830bfacf9cb560d7e04
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2024-06-10 03:05:02 +0900

    refactor: use `@inlinable` rather than `@inline(__always)` as possible

--- Sources/Kernel/mmio.swift
+++ Sources/Kernel/mmio.swift
@@ -1,11 +1,11 @@
 import Volatile
 
-@inline(__always)
+@inlinable
 func mmioLoad(_ address: UInt) -> UInt32 {
     volatile_load(UnsafePointer(bitPattern: address))
 }
 
-@inline(__always)
+@inlinable
 func mmioStore(_ value: UInt32, to address: UInt) {
     volatile_store(UnsafeMutablePointer(bitPattern: address), value)
 }
