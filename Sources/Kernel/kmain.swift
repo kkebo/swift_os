f4d57f9b710de20c7cf04c8ab07e3af7c577a5c1
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2024-09-03 00:00:54 +0900

    fix: force to use `Framebuffer.shared` instead of an arbitary instance
    This prevents the mismatch between `Framebuffer`'s properties and the actual behaviors.

--- Sources/Kernel/kmain.swift
+++ Sources/Kernel/kmain.swift
@@ -4,8 +4,7 @@ public func kmain() {
 
     print("Hello Swift!")
 
-    let fb = Framebuffer()
-    fb.fillRect(x0: 0, y0: 0, x1: 100, y1: 100, color: 0xffffff)
+    Framebuffer.shared.fillRect(x0: 0, y0: 0, x1: 100, y1: 100, color: 0xffffff)
 
     repeat { putchar(getchar()) } while true
 }
