9f52532a98fd74c116868540eafe7db67db1f34c
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2024-11-26 02:29:15 +0900

    feat: add APIs to draw text to a framebuffer

--- Sources/Kernel/kmain.swift
+++ Sources/Kernel/kmain.swift
@@ -6,6 +6,7 @@ public func kmain() {
 
     let fb = Framebuffer(width: 1920, height: 1080, depth: 32, pixelOrder: .rgb)
     fb.fillRect(x0: 0, y0: 0, x1: 100, y1: 100, color: 0xffffff)
+    fb.drawString("Hello Swift!", x: 0, y: 100, color: 0xffffff)
 
     repeat { putchar(getchar()) } while true
 }
