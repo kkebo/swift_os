29856803e4433558a12b5e5be449b87445eeed56
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2025-03-16 13:34:39 +0900

    refactor: use `@main`

---
+++ Sources/Kernel/Kernel.swift
@@ -0,0 +1,13 @@
+@main
+struct Kernel {
+    static func main() {
+        initUART()
+
+        print("Hello Swift!")
+
+        let fb = unsafe Framebuffer(width: 1920, height: 1080, depth: 32, pixelOrder: .rgb)
+        unsafe fb.fillRect(x0: 0, y0: 0, x1: 100, y1: 100, color: 0xffffff)
+
+        repeat { putchar(getchar()) } while true
+    }
+}
