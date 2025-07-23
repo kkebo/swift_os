d5a754490557542ad857b3bb7c25a30c79393419
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2025-07-23 23:17:48 +0900

    ci: use swiftly instead of swiftlang/swift images

---
+++ scripts/ci-install-swift.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install --no-install-recommends -y libcurl4-openssl-dev
+
+curl -O "https://download.swift.org/swiftly/linux/swiftly-$(uname -m).tar.gz"
+tar zxf "swiftly-$(uname -m).tar.gz"
+./swiftly init -y --skip-install -n
+# shellcheck disable=SC1091
+. "$HOME/.local/share/swiftly/env.sh"
+hash -r
+
+echo "PATH=$PATH" >> "$GITHUB_ENV" && echo "SWIFTLY_HOME_DIR=$SWIFTLY_HOME_DIR" >> "$GITHUB_ENV" && echo "SWIFTLY_BIN_DIR=$SWIFTLY_BIN_DIR" >> "$GITHUB_ENV" && echo "SWIFTLY_TOOLCHAINS_DIR=$SWIFTLY_TOOLCHAINS_DIR" >> "$GITHUB_ENV"
+
+curl --silent --retry 3 --location --fail --compressed https://swift.org/keys/all-keys.asc | gpg --import -
+
+swiftly install -y
