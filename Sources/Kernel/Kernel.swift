36d8bbd65b93526c093726a7d359a3d3ddc40dcb
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2026-02-25 01:49:41 +0900

    feat: switch to EL1

--- Sources/Kernel/Kernel.swift
+++ Sources/Kernel/Kernel.swift
@@ -13,7 +13,8 @@ struct Kernel {
             print("Hello Swift!")
 
             print("Exception Level:", terminator: " ")
-            print(get_el())
+            let el = get_el()
+            print(el)
 
             let memoryManager = MemoryManager()
             print("RAM:", terminator: " ")
@@ -23,6 +24,11 @@ struct Kernel {
             var fb = Framebuffer<UInt32>(width: 1920, height: 1080, pixelOrder: .rgb)
             fb.fillRect(x0: 0, y0: 0, x1: 100, y1: 100, color: 0xffffff)
             fb.drawString("Hello Swift!", x: 0, y: 100, color: 0xffffff)
+            let elLabel: StaticString = "Exception Level:"
+            fb.drawString(elLabel, x: 0, y: 108, color: 0xffffff)
+            for i in 0..<Int(el) {
+                fb.drawString("I", x: (elLabel.utf8CodeUnitCount + i) * 8, y: 108, color: 0xffffff)
+            }
 
             repeat { putchar(getchar()) } while true
         #else
