2cdff1c06ab2462b6fc93aeac505c3d4a01051f9
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2026-03-29 04:29:54 +0900

    refactor: move BSS initializing from Assembly to Swift

--- Sources/AsmSupport/include/Support.h
+++ Sources/AsmSupport/include/Support.h
@@ -6,6 +6,8 @@ void delay(uint64_t);
 void halt();
 uintptr_t get_kernel_start();
 uintptr_t get_kernel_end();
+uintptr_t get_bss_start();
+uintptr_t get_bss_end();
 #ifdef __aarch64__
 uint32_t get_el();
 #endif
