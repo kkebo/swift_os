4569ca90dc76ea4e6ef42f60f6215558b9128a25
Kenta Kubo <601636+kkebo@users.noreply.github.com>
2026-05-28 02:24:30 +0900

    feat: add `enableIRQ` and `disableIRQ`

--- Sources/AsmSupport/include/AsmSupport.h
+++ Sources/AsmSupport/include/AsmSupport.h
@@ -4,6 +4,10 @@
 
 void delay(uint64_t);
 void halt(void);
+__attribute__((swift_name("enableIRQ()")))
+void enable_irq(void);
+__attribute__((swift_name("disableIRQ()")))
+void disable_irq(void);
 #ifdef __aarch64__
 __attribute__((swift_name("getEL()")))
 uint32_t get_el(void);
